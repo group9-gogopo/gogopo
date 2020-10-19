0fd5d0fcbfe082afbbad21cf50c450493abba0d4
mzq <1061375978@qq.com>
2020-10-19 21:01:11 +0800

    frame

---
+++ web-front-end/config-overrides.js
@@ -0,0 +1,21 @@
+const path =require('path') 
+const {
+  override,
+  fixBabelImports,
+  addWebpackResolve,
+  addWebpackAlias,
+} = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import ", {
+    libraryName: "antd-mobile",
+    style: "css",
+  }),
+  addWebpackResolve({
+    extensions: [".js", ".json", ".jsx"],
+  }),
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src/"),
+    "@a": path.resolve(__dirname, "src/assets/ "),
+  })
+);
---
+++ web-front-end/src/App.jsx
@@ -0,0 +1,9 @@
+import React ,{Component} from 'react'
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>檀旭在喝水</div>
+        )
+    }
+}
