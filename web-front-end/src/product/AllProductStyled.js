55d29adc890c0df4b3c4a6ed9f0adae5eb8cab12
liyx7 <56989435+liyx7@users.noreply.github.com>
2020-11-07 11:59:08 +0800

    11-7

--- web-front-end/src/product/AllProductStyled.js
+++ web-front-end/src/product/AllProductStyled.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
 const AllProductStyled = styled.div`
-        width:1300px;
-        margin:0 auto;
-        overflow:hidden;
-            div{
-                float:left;
-            
+width:1300px;
+margin:0 auto;
+overflow:hidden;
+    .box{
+        float:left;      
     }
 `
 
